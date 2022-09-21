6efc143f78bb002bed3c82ae971d2fa55e1b4a29
Agajanth <santiagoisaza0@gmail.com>
2022-09-21 14:51:40 -0500

    Add connection method for postgresql, add simple test for connection and utils for save and handle functions and pytest got updated

--- fast_datapy_etl/drivers/postgresql/driver.py
+++ fast_datapy_etl/drivers/postgresql/driver.py
@@ -1,7 +1,8 @@
-from ..base.driver import BaseDriver, BASE_DRIVER
 import pyodbc
-import psycopg3
-from .constants import ODBC_TYPES
+from .constants import ODBC_TYPES, STRING_CONN
+from ...drivers.utils.utils import contains
+from ..base import BaseDriver, BASE_DRIVER
+
 class PostgresDriver(BaseDriver):
 
 
@@ -10,10 +11,33 @@ class PostgresDriver(BaseDriver):
         self.DATA_TYPE = {item.name:item.value for item in ODBC_TYPES}  if base_driver == BASE_DRIVER.ODBC else {}
         super() .__init__(settings_dict, base_driver, alias)
 
-    def get_connection_params(self, **kwg):
+    def get_connection_params(self,IS_DNS:bool=False, **kwg):
+        if IS_DNS and kwg["DNS"] is not None:
+            self.conn_params["DNS"] = kwg["DNS"]
+        if not IS_DNS:
+            check, are_contained = contains(STRING_CONN, **kwg)
+            if are_contained:
+                string_conn_params: list[str] = []
+                for key in kwg.keys():
+                    string_conn_params.append(f"{key}={kwg[key]}")
+                self.conn_params = ";".join(string_conn_params)
+            else:
+                raise Exception(f"These values must be inside de connection string: {check}")
 
 
-    def get_new_connection(self, conn_params):
-        return
 
+    def get_new_connection(self):
+        conn_params = self.conn_params
+        if conn_params is None:
+            raise Exception("The connection string is None")
+        else:
+            if self.base_driver == BASE_DRIVER.ODBC:
+                print(self.conn_params)
+                self.connection = pyodbc.connect(self.conn_params)
+                return self.connection
 
+if __name__ == "__main__":
+    pos = PostgresDriver({},BASE_DRIVER.ODBC,"hi")
+    pos.get_connection_params(User="postgres",DRIVER='{psqlodbcw.so}',Password='admin',Database='postgres',Server='127.0.0.1',Port='5432')
+    con = pos.get_new_connection()
+    print(con)
---
+++ tests/drivers/postgresql/test_postgresql_driver.py
@@ -0,0 +1,9 @@
+from fast_datapy_etl.drivers.postgresql.driver import PostgresDriver
+from fast_datapy_etl.drivers.base.driver import BASE_DRIVER
+import pyodbc
+
+def test_driver_class():
+    pos = PostgresDriver({},BASE_DRIVER.ODBC,"hi")
+    pos.get_connection_params(User="postgres",Driver='{psqlodbcw.so}',Password='admin',Database='postgres',Server='127.0.0.1',Port='5432')
+    con = pos.get_new_connection()
+    assert isinstance(con,pyodbc.Connection)
